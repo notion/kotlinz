2d2a84760f16e608f4a2d21f6d356b0e1dc5964c
Michael Pardo <kensuke155@gmail.com>
2016-06-22 14:34:49 -0400

    Fix args.

--- lib/src/main/kotlin/com/michaelpardo/monads/Composition.kt
+++ lib/src/main/kotlin/com/michaelpardo/monads/Composition.kt
@@ -2,4 +2,4 @@ package com.michaelpardo.monads
 
 infix fun <A, B, C> ((A) -> B).forwardCompose(f: (B) -> C): ((A) -> C) = { a: A -> f(this(a)) }
 
-infix fun <A, B, C> ((B) -> C).compose(f: (A) -> B): ((A) -> C) = { b: B -> this(f(b)) }
+infix fun <A, B, C> ((B) -> C).compose(f: (A) -> B): ((A) -> C) = { a: A -> this(f(a)) }
